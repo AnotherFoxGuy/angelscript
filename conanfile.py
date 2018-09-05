f264546f104b6d4e94ada030a5fe8bd766df9bcd
Edgar <Edgar@AnotherFoxGuy.com>
2018-09-05 19:35:19 +0200

    Fixed missing lib

--- conanfile.py
+++ conanfile.py
@@ -22,9 +22,9 @@ class AngelscriptConan(ConanFile):
         self.copy("*.h", dst="include", src="include")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so*", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["angelscript"]
+        self.cpp_info.libs = tools.collect_libs(self)
